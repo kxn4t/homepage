83729980383e782fbc3cb7047e60d3198e84f609
kxn4t <kxxn4t@gmail.com>
2020-03-08 02:47:09 +0900

    add darkmode

--- components/atoms/Card.vue
+++ components/atoms/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="relative mx-0 my-2 sm:m-2 bg-white border border-gray-200 rounded-diagonal shadow-lg overflow-hidden"
+      class="relative mx-0 my-2 sm:m-2 bg border border-color rounded-diagonal shadow-lg overflow-hidden"
     >
       <!-- image -->
       <img
@@ -36,7 +36,7 @@
           <span
             v-for="(tag, key, index) in tags"
             :key="index"
-            class="inline py-1 px-2 mr-1 rounded-full text-3xs text-gray-700 bg-gray-300"
+            class="inline py-1 px-2 mr-1 rounded-full text-3xs tag-text tag-bg"
           >
             {{ '#' + tag }}
           </span>
@@ -84,6 +84,26 @@ export default {
 </script>
 
 <style scoped>
+.bg {
+  background-color: #fff;
+  background-color: var(--main-bg);
+}
+
+.border-color {
+  border-color: #edf2f7;
+  border-color: var(--border-color);
+}
+
+.tag-text {
+  color: #4a5568;
+  color: var(--tag-text);
+}
+
+.tag-bg {
+  background-color: #e2e8f0;
+  background-color: var(--tag-bg);
+}
+
 .rounded-diagonal {
   border-top-left-radius: 1.75rem;
   border-top-right-radius: 0.5rem;
